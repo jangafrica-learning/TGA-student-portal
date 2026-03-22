4826547f043bd0e78cd4397a162a75c07c60af57
JangAfrica Learning Centre <info.jangafrica@gmail.com>
2026-03-22 09:03:59 +0000

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = "tga-rc-v1";
+
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/folders_access.html",
+  "/student_hub.html",
+  "/student_request.html",
+  "/manifest.json",
+  "/logo.png"
+];
+
+// Install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch (Network first, fallback to cache)
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+});
